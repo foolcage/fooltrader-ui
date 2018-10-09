5a68cb027fa571ec7b19818bd0d9bfb4c933312e
foolcage <5533061@qq.com>
2018-10-09 18:08:34 +0800

    add vue-echarts

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="container">
+        <BaseInputText
+                v-model="inputText"
+                placeholder="New todo"
+                @keydown.enter="addTodo"/>
+
         <table class="table table-striped">
             <thead>
             <tr>
+                <th>trader</th>
                 <th>ID</th>
                 <th>Name</th>
                 <th>Price</th>
@@ -10,26 +16,48 @@
             </thead>
             <tbody>
             <tr v-for="coin in coins" :key="coin.id">
+                <td>{{ trader }}</td>
                 <td>{{ coin.id }}</td>
                 <td>{{ coin.name }}</td>
                 <td>{{ coin.price }}</td>
             </tr>
             </tbody>
         </table>
+        <MyChart/>
     </div>
 </template>
 
 <script>
+    import BaseInputText from './BaseInputText.vue'
+    import MyChart from './MyChart.vue'
     import {mapState} from 'vuex'
 
     export default {
+        components: {
+            BaseInputText, MyChart
+        },
+
         name: 'HelloWorld',
         mounted() {
-            setInterval(() => { this.$store.dispatch('loadCoins')}, 1000);
+            setInterval(() => {
+                this.$store.dispatch('loadCoins')
+            }, 1000);
         },
         computed: mapState([
             'coins'
-        ])
+        ]),
+        data() {
+            return {
+                'inputText': '',
+                'trader': ''
+            }
+        },
+
+        methods: {
+            addTodo() {
+                this.trader = this.inputText.trim()
+            }
+        }
     }
 
 </script>
